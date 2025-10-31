03c4c3f4d74e9013e3a784f2fddcb93ce596ba3f
phucbaehe186911-boop <phucbaehe186911@gmail.com>
2025-11-01 00:20:31 +0700

    Add Shipping.java

---
+++ src/java/model/Shipping.java
@@ -0,0 +1,100 @@
+package model;
+
+import java.sql.Timestamp;
+
+public class Shipping {
+    private int id;
+    private int orderId;
+    private int shipperId;
+    private String deliveryAddress;
+    private Timestamp deliveryDate;
+    private String status;
+    private double shippingFee;
+    private String paymentStatus;
+
+    private String customerName; // Dùng để hiển thị
+    private Timestamp orderDate; // Dùng để hiển thị
+
+    // Getters, Setters, Constructors
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getOrderId() {
+        return orderId;
+    }
+
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
+
+    public int getShipperId() {
+        return shipperId;
+    }
+
+    public void setShipperId(int shipperId) {
+        this.shipperId = shipperId;
+    }
+
+    public String getDeliveryAddress() {
+        return deliveryAddress;
+    }
+
+    public void setDeliveryAddress(String deliveryAddress) {
+        this.deliveryAddress = deliveryAddress;
+    }
+
+    public Timestamp getDeliveryDate() {
+        return deliveryDate;
+    }
+
+    public void setDeliveryDate(Timestamp deliveryDate) {
+        this.deliveryDate = deliveryDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public double getShippingFee() {
+        return shippingFee;
+    }
+
+    public void setShippingFee(double shippingFee) {
+        this.shippingFee = shippingFee;
+    }
+
+    public String getPaymentStatus() {
+        return paymentStatus;
+    }
+
+    public void setPaymentStatus(String paymentStatus) {
+        this.paymentStatus = paymentStatus;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    public Timestamp getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Timestamp orderDate) {
+        this.orderDate = orderDate;
+    }
+    
+}
